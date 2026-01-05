32f91b03d11d51709f9b4266071de432423c85bd
Johan Westling <johan@westling.ax>
2026-01-05 11:11:55 +0000

    Corrected host path for ssh config copy

--- .devcontainer/lifecycle/postAttachCommand.sh
+++ .devcontainer/lifecycle/postAttachCommand.sh
@@ -5,3 +5,5 @@ DEVCONTAINER_SCRIPT_PATH="$(dirname $DEVCONTAINER_LIFECYCLE_PATH)/script"
 
 echo -en "\033[1;33m⚬\033[0m "
 echo -e "\033[90mpostAttachCommand.sh\033[0m"
+
+exec bash
--- .devcontainer/script/sshConfigCopy.sh
+++ .devcontainer/script/sshConfigCopy.sh
@@ -2,7 +2,7 @@
 
 echo -en "\033[1;33m→\033[0m "
 echo -e "Copy ssh config from host mount"
-\cp -R /root/host/.ssh/ /root
+\cp -R /root/.host/.ssh/ /root
 
 if [ -n "$(grep 'UseKeychain' /root/.ssh/config &> /dev/null)" ]; then
 	echo -en "\033[1;33m→\033[0m "
