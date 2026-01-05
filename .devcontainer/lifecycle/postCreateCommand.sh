541af4639bf709427fbb04d9040e1ffdc9582769
Johan Westling <johan@westling.ax>
2026-01-05 11:27:23 +0000

    Work in progress

--- .devcontainer/lifecycle/postCreateCommand.sh
+++ .devcontainer/lifecycle/postCreateCommand.sh
@@ -7,6 +7,7 @@ echo -en "\033[1;33m⚬\033[0m "
 echo -e "\033[90mpostCreateCommand.sh\033[0m"
 
 source $DEVCONTAINER_SCRIPT_PATH/bashCompletion.sh
+source $DEVCONTAINER_SCRIPT_PATH/bashAliases.sh
 source $DEVCONTAINER_SCRIPT_PATH/sshConfigCopy.sh
 source $DEVCONTAINER_SCRIPT_PATH/gitConfigCopy.sh
 source $DEVCONTAINER_SCRIPT_PATH/gitConfigEditor.sh
