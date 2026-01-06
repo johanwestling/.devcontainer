782819059a3389a6c9ee8ff8c63b5568e429c50d
Johan Westling <johan@westling.ax>
2026-01-06 15:20:31 +0200

    Relative path

--- devcontainerUpdate.sh
+++ devcontainerUpdate.sh
@@ -3,4 +3,4 @@
 echo -en "\033[1;33m→\033[0m "
 echo -e "Update .devcontainer"
 
-git subtree pull --prefix .devcontainer git@github.com:johanwestling/.devcontainer.git main --squash
+git subtree pull --prefix ./.devcontainer git@github.com:johanwestling/.devcontainer.git main --squash
