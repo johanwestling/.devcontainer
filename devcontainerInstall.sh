1394e5912dda4dc95d6fe033b2b5353a4b847030
Johan Westling <johan@westling.ax>
2026-01-06 15:23:59 +0200

    Work in progress

--- devcontainerInstall.sh
+++ devcontainerInstall.sh
@@ -3,4 +3,4 @@
 echo -en "\033[1;33m→\033[0m "
 echo -e "Install .devcontainer"
 
-git subtree add --prefix ./.devcontainer git@github.com:johanwestling/.devcontainer.git main --squash
+git subtree add --prefix .devcontainer git@github.com:johanwestling/.devcontainer.git main --squash
